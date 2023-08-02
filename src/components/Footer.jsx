16a7df169071fd26558fcf4ee1be8cc71463aa54
PVSKR <skr.polu@hotmail.com>
2023-08-02 23:11:59 +0530

    header for laptop layout

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
-      <div className="footer-content">footer</div>
+      <div className="footer-content">....</div>
     </footer>
   );
 }
