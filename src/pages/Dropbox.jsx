f42b99a0a27900d78ee61f81e945c01de05bae95
PVSKR <skr.polu@hotmail.com>
2023-08-04 01:29:41 +0530

    bblock1 font styling

--- src/pages/Dropbox.jsx
+++ src/pages/Dropbox.jsx
@@ -12,34 +12,31 @@ export default function Dropbox() {
             Easy to use, reliable, private, and secure. It’s no wonder Dropbox
             is the choice for storing and sharing your most important files.
           </p>
-          <nav>
-            <NavLink className="find-your-plan" to="">
-              Find your plan
-              <span>
-                <svg
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  class="dig-UIIcon dig-UIIcon--standard"
-                  width="24"
-                  height="24"
-                  role="presentation"
-                  focusable="false"
-                >
-                  <path
-                    d="M5 11.75h12m-5.25-6.5 6.25 6.5-6.25 6.5"
-                    stroke="currentColor"
-                    stroke-width="1.5"
-                    stroke-miterlimit="10"
-                    vector-effect="non-scaling-stroke"
-                  ></path>
-                </svg>
-              </span>
-            </NavLink>
-          </nav>
         </div>
-
+        <NavLink className="find-your-plan" to="">
+          Find your plan
+          <span>
+            <svg
+              viewBox="0 0 24 24"
+              fill="none"
+              class="dig-UIIcon dig-UIIcon--standard"
+              width="24"
+              height="24"
+              role="presentation"
+              focusable="false"
+            >
+              <path
+                d="M5 11.75h12m-5.25-6.5 6.25 6.5-6.25 6.5"
+                stroke="currentColor"
+                stroke-width="1.5"
+                stroke-miterlimit="10"
+                vector-effect="non-scaling-stroke"
+              ></path>
+            </svg>
+          </span>
+        </NavLink>
         <video
-          className="video dwg-media-video dwg-media-video--object-fit-cover dwg-box dwg-display--block dwg-position--absolute dwg-width--full dwg-height--full"
+          className="video-my"
           // aria-hidden="false"
           aria-label="Dropbox"
           autoPlay
