43a7e6fa41962286f5d9af557465ddd49a7c4d96
PVSKR <skr.polu@hotmail.com>
2023-08-04 08:28:36 +0530

    dwa-flex item add to block1 and  styled.

--- src/pages/Dropbox.jsx
+++ src/pages/Dropbox.jsx
@@ -48,6 +48,63 @@ export default function Dropbox() {
           {/* <source src={bgvid} type="video/webm; codecs=vp9" />
           <source src={bgvid} type="video/quicktime; codecs=hvc1" /> */}
         </video>
+        <div className="wda">
+          <h2>See what sets Dropbox apart</h2>
+          <div className="wda-flex">
+            <div className="wda-info-flex">
+              <picture className="wda-piture">
+                <img
+                  src="https://fjord.dropboxstatic.com/warp/conversion/dropbox/warp/en-us/dropbox/emoji smile.svg?id=42f38245-8ec6-4e1f-8519-c66d296b1cbf"
+                  className="wda-img"
+                ></img>
+              </picture>
+              <h3 className="wda-h3">Your files, not ours</h3>
+              <p className="wda-p">
+                With Dropbox, your files belong to you, not us, so you can be
+                sure we’re not reselling your data.
+              </p>
+            </div>
+            <div className="wda-info-flex">
+              <picture className="wda-piture">
+                <img
+                  src="https://fjord.dropboxstatic.com/warp/conversion/dropbox/warp/en-us/dropbox/cloud done.svg?id=f86f48e9-1a60-4f75-9075-79cd479ddb2b"
+                  className="wda-img"
+                ></img>
+              </picture>
+              <h3 className="wda-h3">All your files in one place</h3>
+              <p className="wda-p">
+                Store computer backups, photo libraries, thousands of
+                documents—all your files, in the same place.
+              </p>
+            </div>
+            <div className="wda-info-flex">
+              <picture className="wda-piture">
+                <img
+                  src="https://fjord.dropboxstatic.com/warp/conversion/dropbox/warp/en-us/dropbox/twinkle.svg?id=4d4d29e5-c87f-416b-a3ac-583eb86fc627"
+                  className="wda-img"
+                ></img>
+              </picture>
+              <h3 className="wda-h3">One-of-a-kind functionality</h3>
+              <p className="wda-p">
+                Store files as large as 2 TB each, sync at light speed, and
+                allow anyone to preview nearly 200 file types.
+              </p>
+            </div>
+            <div className="wda-info-flex">
+              <picture className="wda-piture">
+                <img
+                  src="https://fjord.dropboxstatic.com/warp/conversion/dropbox/warp/en-us/dropbox/shield.svg?id=bda0e9da-566d-4941-91a6-05b39f678c8d"
+                  className="wda-img"
+                ></img>
+              </picture>
+              <h3 className="wda-h3">Ease of use and security</h3>
+              <p className="wda-p">
+                2/3 of surveyed users say Dropbox keeps their files more secure
+                and we’re a leader in ease of use.
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
       <div className="block2"></div>
       <div className="block3"></div>
