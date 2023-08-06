e20b5482aff1f637fa0bf70bf7cfc57c1a741363
PVSKR <skr.polu@hotmail.com>
2023-08-06 18:46:31 +0530

    svg added to the section one of block4 class

--- src/pages/Dropbox.jsx
+++ src/pages/Dropbox.jsx
@@ -515,6 +515,23 @@ export default function Dropbox() {
                 anyone. Discover what Dropbox can do for you- get a free
                 account, no strings attached!
               </p>
+              <svg
+                viewBox="0 0 24 24"
+                fill="none"
+                class="dig-UIIcon dig-UIIcon--standard"
+                width="24"
+                height="24"
+                role="presentation"
+                focusable="false"
+              >
+                <path
+                  d="m5.25 9.25 6.5 6.25 6.5-6.25"
+                  stroke="currentColor"
+                  stroke-width="1.5"
+                  stroke-miterlimit="10"
+                  vector-effect="non-scaling-stroke"
+                ></path>
+              </svg>
               <Link className="asp-link">
                 Learn more
                 <span>
