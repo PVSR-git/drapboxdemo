04e7014f5a5566663f200cf78b32f6ad8ef3b9b5
PVSKR <skr.polu@hotmail.com>
2023-08-10 00:05:31 +0530

    footer react section completed.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,59 @@ import React from "react";
 export default function Footer() {
   return (
     <footer>
-      <div className="footer-content">....</div>
+      <div className="footer-content">
+        <div className="footer-grid">
+          <div className="footer-grid-info">
+            <h2>Dropbox</h2>
+            <h4>Desktop app</h4>
+            <h4>Integrations</h4>
+            <h4>Features</h4>
+            <h4>Solutions</h4>
+            <h4>Do more than store</h4>
+            <h4>Security</h4>
+            <h4>Advance access</h4>
+          </div>
+          <div className="footer-grid-info">
+            <h2>Products</h2>
+            <h4>Plus</h4>
+            <h4>Professional</h4>
+            <h4>Business</h4>
+            <h4>Enterprise</h4>
+            <h4>Dropbox Sign</h4>
+            <h4>DocSend</h4>
+            <h4>Plans</h4>
+            <h4>Product updates</h4>
+          </div>
+          <div className="footer-grid-info">
+            <h2>Support</h2>
+            <h4>Help center</h4>
+            <h4>Contact us</h4>
+            <h4>Privacy & terms</h4>
+            <h4>Cookie police</h4>
+            <h4>Cookies & CCPA preferences</h4>
+            <h4>AI principles</h4>
+            <h4>Sitemap</h4>
+            <h4>Learning resources</h4>
+          </div>
+          <div className="footer-grid-info">
+            <h2>Community</h2>
+            <h4>Blog</h4>
+            <h4>Developers</h4>
+            <h4>Community forums</h4>
+            <h4>Referrals</h4>
+            <h4>Reseller partners</h4>
+            <h4>Intergration partners</h4>
+          </div>
+          <div className="footer-grid-info">
+            <h2>Company</h2>
+            <h4>About us</h4>
+            <h4>Jobs</h4>
+            <h4>Investor relations</h4>
+            <h4>ESG</h4>
+            <h4>Find a partner</h4>
+          </div>
+        </div>
+      </div>
     </footer>
   );
 }
