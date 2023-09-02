d92da69ffcf956a810463fdefbdbc7ea5e55c35f
PVSKR <skr.polu@hotmail.com>
2023-09-02 15:30:57 +0530

    The accordion item changes automatically, causing the image to frequently change. test 5

--- src/pages/Dropbox.jsx
+++ src/pages/Dropbox.jsx
@@ -546,8 +546,8 @@ export default function Dropbox() {
             </p>
           </div>
         </div>
-        <div className="block4-grid">
-          {data.map((item, i) => (
+        {data.map((item, i) => (
+          <div className="block4-grid">
             <div className="block4-flex-child1" key={i}>
               <div className="wrapper">
                 <section className="accordion">
@@ -607,18 +607,18 @@ export default function Dropbox() {
                   </div>
                 </section>
               </div>
-              <div className="block4-flex-child2">
-                <img
-                  className={selected === i ? "content show" : "content"}
-                  alt="Image of files and folders that are organized and stored in Dropbox"
-                  loading="lazy"
-                  src={selected === i ? item.selectedSrc : item.src}
-                  srcSet={item.srcset}
-                />
-              </div>
             </div>
-          ))}
-        </div>
+            <div className="block4-flex-child2">
+              <img
+                className={selected === i ? "content show" : "content"}
+                alt="Image of files and folders that are organized and stored in Dropbox"
+                loading="lazy"
+                // src={selected === i ? item.selectedSrc : item.src}
+                srcSet={item.srcset}
+              />
+            </div>
+          </div>
+        ))}
       </div>
       <div className="block5">
         <div className="block5-text">
