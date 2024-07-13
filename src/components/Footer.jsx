859925816760ebcb3c0b3c2719ebfcc987326e8e
PVSKR <skr.polu@hotmail.com>
2024-07-13 22:34:03 +0530

    one more bug fixed.

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -144,23 +144,20 @@ export default function Footer() {
               target="_blank"
               aria-label="Twitter"
               data-uxa-log="warp_footer_social_media_twitter"
-              data-trackingid="warp_footer_social_media_twitter"
-            >
-              <span class="dwg-lazy-icon--large dwg-box dwg-display--inline-block">
+              data-trackingid="warp_footer_social_media_twitter">
+              <span className="dwg-lazy-icon--large dwg-box dwg-display--inline-block">
                 <svg
                   viewBox="0 0 24 24"
                   fill="none"
-                  class="dig-UIIcon"
+                  className="dig-UIIcon"
                   width="32"
                   height="32"
                   role="presentation"
-                  focusable="false"
-                >
+                  focusable="false">
                   <path
                     d="M9.026 18.51c6.038 0 9.341-5.007 9.341-9.341 0-.141 0-.282-.006-.423A6.689 6.689 0 0 0 20 7.043a6.654 6.654 0 0 1-1.889.519 3.303 3.303 0 0 0 1.447-1.819 6.53 6.53 0 0 1-2.087.794A3.277 3.277 0 0 0 15.076 5.5a3.287 3.287 0 0 0-3.284 3.285c0 .256.032.505.083.749a9.323 9.323 0 0 1-6.767-3.432 3.292 3.292 0 0 0 1.018 4.386 3.32 3.32 0 0 1-1.486-.41v.045a3.29 3.29 0 0 0 2.632 3.22 3.198 3.198 0 0 1-.865.115c-.21 0-.416-.019-.614-.057a3.283 3.283 0 0 0 3.067 2.28 6.585 6.585 0 0 1-4.079 1.408A6.32 6.32 0 0 1 4 17.044a9.339 9.339 0 0 0 5.026 1.466Z"
                     fill="currentColor"
-                    vector-effect="non-scaling-stroke"
-                  ></path>
+                    vector-effect="non-scaling-stroke"></path>
                 </svg>
               </span>
             </a>
@@ -172,23 +169,20 @@ export default function Footer() {
               target="_blank"
               aria-label="Facebook"
               data-uxa-log="warp_footer_social_media_facebook"
-              data-trackingid="warp_footer_social_media_facebook"
-            >
-              <span class="dwg-lazy-icon--large dwg-box dwg-display--inline-block">
+              data-trackingid="warp_footer_social_media_facebook">
+              <span className="dwg-lazy-icon--large dwg-box dwg-display--inline-block">
                 <svg
                   viewBox="0 0 24 24"
                   fill="none"
-                  class="dig-UIIcon"
+                  className="dig-UIIcon"
                   width="32"
                   height="32"
                   role="presentation"
-                  focusable="false"
-                >
+                  focusable="false">
                   <path
                     d="M20 12a8 8 0 0 0-8-8 8.001 8.001 0 0 0-1.25 15.903v-5.59H8.719V12h2.031v-1.762c0-2.005 1.194-3.113 3.022-3.113.875 0 1.79.156 1.79.156V9.25h-1.009c-.994 0-1.303.617-1.303 1.25V12h2.219l-.355 2.313H13.25v5.59A8.001 8.001 0 0 0 20 12Z"
                     fill="currentColor"
-                    vector-effect="non-scaling-stroke"
-                  ></path>
+                    vector-effect="non-scaling-stroke"></path>
                 </svg>
               </span>
             </a>
@@ -200,25 +194,22 @@ export default function Footer() {
               target="_blank"
               aria-label="Youtube"
               data-uxa-log="warp_footer_social_media_youtube"
-              data-trackingid="warp_footer_social_media_youtube"
-            >
-              <span class="dwg-lazy-icon--large dwg-box dwg-display--inline-block">
+              data-trackingid="warp_footer_social_media_youtube">
+              <span className="dwg-lazy-icon--large dwg-box dwg-display--inline-block">
                 <svg
                   viewBox="0 0 24 24"
                   fill="none"
-                  class="dig-UIIcon"
+                  className="dig-UIIcon"
                   width="32"
                   height="32"
                   role="presentation"
-                  focusable="false"
-                >
+                  focusable="false">
                   <path
                     fill-rule="evenodd"
                     clip-rule="evenodd"
                     d="M19.148 7.358c.249.248.427.557.519.896.329 1.248.333 3.851.333 3.851s0 2.604-.333 3.852a2.006 2.006 0 0 1-1.415 1.414c-1.247.335-6.252.335-6.252.335s-5.004 0-6.25-.335a2.004 2.004 0 0 1-1.415-1.414C4 14.709 4 12.105 4 12.105s0-2.603.335-3.85a2.003 2.003 0 0 1 1.414-1.42C6.995 6.5 12 6.5 12 6.5s5.004 0 6.252.34c.34.091.648.27.896.518Zm-4.59 4.747-4.158-2.4v4.8l4.158-2.4Z"
                     fill="currentColor"
-                    vector-effect="non-scaling-stroke"
-                  ></path>
+                    vector-effect="non-scaling-stroke"></path>
                 </svg>
               </span>
             </a>
@@ -231,36 +222,32 @@ export default function Footer() {
               <svg
                 viewBox="0 0 24 24"
                 fill="none"
-                class="dig-UIIcon dig-UIIcon--standard"
+                className="dig-UIIcon dig-UIIcon--standard"
                 width="24"
                 height="24"
                 role="presentation"
-                focusable="false"
-              >
+                focusable="false">
                 <path
                   d="M11.75 4C6.535 4 4 6.535 4 11.75s2.535 7.75 7.75 7.75 7.75-2.535 7.75-7.75S16.965 4 11.75 4Zm5.714 4.5H14.87a9.237 9.237 0 0 0-.623-2.711A4.454 4.454 0 0 1 17.463 8.5ZM18 11.75c.002.586-.042 1.171-.133 1.75H14.97c.022-.579.03-1.167.03-1.75s-.008-1.171-.03-1.75h2.897c.09.579.135 1.164.133 1.75ZM11.75 18c-.616 0-1.3-.41-1.598-3h3.196c-.298 2.59-.982 3-1.598 3Zm-1.712-4.5c-.024-.524-.038-1.1-.038-1.75 0-.65.014-1.226.038-1.75h3.424c.024.524.038 1.1.038 1.75 0 .65-.014 1.226-.038 1.75h-3.424ZM5.5 11.75c-.002-.586.042-1.171.133-1.75H8.53c-.022.579-.03 1.167-.03 1.75s.008 1.171.03 1.75H5.633a11.074 11.074 0 0 1-.133-1.75Zm6.25-6.25c.616 0 1.3.41 1.598 3h-3.196c.298-2.59.982-3 1.598-3Zm-2.497.289A9.236 9.236 0 0 0 8.63 8.5H6.036a4.454 4.454 0 0 1 3.217-2.711ZM6.036 15H8.63c.07.93.28 1.844.623 2.711A4.453 4.453 0 0 1 6.037 15Zm8.21 2.711A9.237 9.237 0 0 0 14.87 15h2.594a4.454 4.454 0 0 1-3.217 2.711Z"
                   fill="currentColor"
-                  vector-effect="non-scaling-stroke"
-                ></path>
+                  vector-effect="non-scaling-stroke"></path>
               </svg>
               English (United States)
-              <span class="dwg-box dwg-display--flex dwg-pl--1">
+              <span className="dwg-box dwg-display--flex dwg-pl--1">
                 <svg
                   viewBox="0 0 24 24"
                   fill="none"
-                  class="dig-UIIcon dig-UIIcon--standard"
+                  className="dig-UIIcon dig-UIIcon--standard"
                   width="24"
                   height="24"
                   role="presentation"
-                  focusable="false"
-                >
+                  focusable="false">
                   <path
                     d="m9.25 5.75 6.25 6.5-6.25 6.5"
                     stroke="currentColor"
                     stroke-width="1.5"
                     stroke-miterlimit="10"
-                    vector-effect="non-scaling-stroke"
-                  ></path>
+                    vector-effect="non-scaling-stroke"></path>
                 </svg>
               </span>
             </span>
